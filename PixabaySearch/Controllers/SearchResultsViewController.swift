4bcf8a16b0ce30574f57906366e3b036b33f4148
Andras Pal <andr4s.pal@gmail.com>
2021-04-15 19:17:06 +0100

    Move `findImages` to the searchResultVC, update navigation

--- PixabaySearch/Controllers/SearchResultsViewController.swift
+++ PixabaySearch/Controllers/SearchResultsViewController.swift
@@ -9,8 +9,28 @@ import UIKit
 
 class SearchResultsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
+    var searchString = String()
+    private var imageArray = [ImageInfo]()
+    private var tempLabel = UILabel()
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .bgColour
+        view.addSubview(tempLabel)
+        tempLabel.text = searchString
+        
+        tempLabel.translatesAutoresizingMaskIntoConstraints = false
+        tempLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        tempLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        tempLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavbar()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -25,5 +45,26 @@ class SearchResultsViewController: UIViewController, UICollectionViewDataSource,
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    private func setupNavbar() {
+        navigationItem.title = "Gallery"
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+    
+    private func findImages(query: String) {
+        NetworkService.shared.fetchImages(query: query, amount: 10) { (result) in
+            switch result {
+            case let .failure(error):
+                let alerController = UIAlertController(title: "Error", message: "Detailed error messages are not implemented", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .destructive) { _ in }
+                alerController.addAction(okAction)
+                self.present(alerController, animated: true, completion: nil)
+                print (error)
+            case let .success(imageData):
+                self.imageArray = imageData
+                print("images: \(self.imageArray[0])")
+            }
+        }
     }
 }
--- PixabaySearch/Controllers/SearchViewController.swift
+++ PixabaySearch/Controllers/SearchViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
-    private var imageArray = [ImageInfo]()
     private let searchView = SearchView()
     
     override func loadView() {
@@ -18,54 +17,35 @@ class SearchViewController: UIViewController {
     }
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         setupNavbar()
     }
-    
-    private func findImages(query: String) {
-        
-        NetworkService.shared.fetchImages(query: query, amount: 10) { (result) in
-            switch result {
-            case let .failure(error):
-                let alerController = UIAlertController(title: "Error", message: "Detailed error messages are not implemented", preferredStyle: .alert)
-                let okAction = UIAlertAction(title: "OK", style: .destructive) { _ in }
-                alerController.addAction(okAction)
-                self.present(alerController, animated: true, completion: nil)
-                print (error)
-            case let .success(imageData):
-                self.imageArray = imageData
-                print("images: \(self.imageArray[0])")
-            }
-        }
-    }
-    
-//    private func setupSearchController() {
-//        let searchController = UISearchController(searchResultsController: nil)
-//        searchController.searchBar.placeholder = "Enter keywords to find images"
-//        searchController.searchBar.delegate = self
-//        navigationItem.searchController = searchController
-//    }
     
     private func setupNavbar() {
         navigationItem.title = "Pixabay images"
     }
     
+    private func updateNavbar() {
+        let backButton = UIBarButtonItem()
+        backButton.title = "Back"
+        backButton.tintColor = .mainTextColour
+        navigationItem.backBarButtonItem = backButton
+        navigationItem.title = ""
+    }
+    
     private func handleButtonTap(_ customView: SearchView) {
-        print("tap")
+        guard let searchString = searchView.textView.text else {
+            print("error with the searchString")
+            return
+        }
+        if searchString != "" {
+            let searchResultsVC = SearchResultsViewController()
+            self.navigationController?.pushViewController(searchResultsVC, animated: true)
+            searchResultsVC.searchString = searchString
+            self.updateNavbar()
+        }
     }
 }
-
-//extension SearchViewController: UISearchBarDelegate {
-//
-//    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
-//        return true
-//    }
-//
-//    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-//        guard let query = searchBar.text else {
-//            return
-//        }
-//        findImages(query: query)
-//    }
-//}
